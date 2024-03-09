4a9e86fce88bce107f1b971d3760dc09255c2a64
Rana0001 <magarprashant001@gmail.com>
2024-03-09 12:51:17 +0545

    Bloc and Cubit in Counter App with bloc State Testing

---
+++ lib/presentation/screen/counter_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_practice/business_logics/cubit/counter_cubit.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    backgroundColor: Colors.green,
+                    content: Center(
+                      child: Text(
+                        "Increment",
+                      ),
+                    ),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                } else if (state.wasIncremented == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    backgroundColor: Colors.redAccent,
+                    content: Center(
+                      child: Text(
+                        "Decrement",
+                      ),
+                    ),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                return Text(
+                  '${state.counterValue}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).increment();
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).decrement();
+            },
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ test/cubit_test/counter_cubit_test.dart
@@ -0,0 +1,42 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc_practice/business_logics/cubit/counter_cubit.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group("Counter Cubit Test", () {
+    CounterCubit? counterCubit;
+    setUp(() {
+      counterCubit = CounterCubit();
+    });
+
+    tearDown(() {
+      counterCubit?.close();
+    });
+
+    test(
+        'The initial value for the counterCubit is CounterState(counterValue:0, wasIncremented: false)',
+        () {
+      expect(counterCubit?.state,
+          CounterState(counterValue: 0, wasIncremented: false));
+    });
+
+    blocTest(
+      "The cubit should emit a CounterState(counterValue:1, wasIncremented: true) when cubit.incremented function is called",
+      build: () => counterCubit!,
+      expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+      act: (cubit) {
+        cubit.increment();
+      },
+    );
+
+    blocTest(
+      "The cubit should emit a CounterState(counterValue:-1, wasIncremented: false) when cubit.decremented function is called",
+      build: () => counterCubit!,
+      expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+      act: (cubit) {
+        cubit.decrement();
+      },
+    );
+  });
+}
