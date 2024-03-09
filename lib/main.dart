e3b434831518d85a83e4454092faf943e6727b91
Rana0001 <magarprashant001@gmail.com>
2024-03-09 14:19:59 +0545

    Wraping the Material App with Bloc Provider to access state globally

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,26 @@ import 'package:flutter_bloc_practice/presentation/screen/second_screen.dart';
 import 'package:flutter_bloc_practice/presentation/screen/third_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _appRouter.dispose();
   }
 }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -6,35 +6,22 @@ import 'package:flutter_bloc_practice/presentation/screen/second_screen.dart';
 import 'package:flutter_bloc_practice/presentation/screen/third_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
   Route onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case "/":
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: const CounterScreen(title: "Counter Screen"),
-                ));
+            builder: (_) => const CounterScreen(title: "Counter Screen"));
 
       case "/second":
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: const SecondScreen(
-                    title: "SecondScreen",
-                  ),
+            builder: (_) => const SecondScreen(
+                  title: "SecondScreen",
                 ));
       case "/third":
         return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                value: _counterCubit,
-                child: const ThirdScreen(title: "Third Screen")));
+            builder: (_) => const ThirdScreen(title: "Third Screen"));
       default:
         throw Exception("404-Not-Found");
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
